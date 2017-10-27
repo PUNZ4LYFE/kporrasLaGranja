7325aebd2c0dab61087a6ce78ba161649c13d5f2
Esteban Padilla <esteban.padilla@fairplaylabs.com>
2017-10-26 21:00:30 -0600

    Save new Vaca.

--- js/animal/Vaca.js
+++ js/animal/Vaca.js
@@ -1,26 +1,21 @@
 var Vaca = (
 	function () {
-		function Vaca(/*pnombre, pedad, paltura, ppeso, ptamanno*/){
-            /*this.nombre = pnombre;
-            this.edad = pedad;
-            this.altura = paltura;
-            this.peso = ppeso;
-            this.tamanno = ptamanno;*/
-            }
-            
-            //Class Methods
-            
-		/*Vaca.prototype.comer = function () {
-			console.log(this.nombre + ': soy una vaca & me gusta el pasto!');
-        }
 
-        Vaca.prototype.caminar = function () {
-			console.log(this.nombre + ': soy una vaca & casi no camino.');
-        }
+		function Vaca(pnombre) {
+			Animal.call(this, pnombre);
+		}
+		//Heredar los metodos definidos en Animal (prototype)
+		Vaca.prototype = Object.create(Animal.prototype);
+		Vaca.prototype.constructor = Animal;
 
-        Vaca.prototype.beber = function () {
-			console.log(this.nombre + ': soy una vaca & me gusta beber agua.');
-        }*/ 
+		//Class Methods
+		Vaca.prototype.comer = function () {
+			console.log(this.nombre + ' soy una vaca y estoy comiendo.');
+		}
+
+		Vaca.prototype.brincar = function () {
+			console.log(this.nombre + ' soy una vaca y estoy comiendo.');
+		}
 
 		return Vaca;
 	}
