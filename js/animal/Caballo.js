6e072fa05ae6c7a1335fa80b87bb40d94ea0461e
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-08 18:30:41 -0600

    Update Caballo.js

--- js/animal/Caballo.js
+++ js/animal/Caballo.js
@@ -1,9 +1,32 @@
 var Caballo = (
 	function () {
-		function Caballo() {
+		function Caballo(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
+                  Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
+                  this.color;
+            /*this.nombre = pnombre;
+            this.edad = pedad;
+            this.altura = paltura;
+            this.peso = ppeso;
+            this.tamanno = ptamanno;*/
+            }
+
+            //Herencia
+            Caballo.prototype = Object.create(Animal.prototype);
+            Caballo.prototype.constructor = Animal;
             
-		}
 		//Class Methods
+            
+            Caballo.prototype.comer = function () {
+			console.log(this.nombre + ': soy un caballo & solo como zanahorias!');
+        }
+
+        Caballo.prototype.caminar = function () {
+			console.log(this.nombre + ': soy un caballo & camino con estilo!');
+        }
+
+        Caballo.prototype.beber = function () {
+			console.log(this.nombre + ': soy un caballo & solo bebo agua!');
+        }      
 
 		return Caballo;
 	}
