da8e02d018ebec828c7bd41159aa32fc7a5e5e7c
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-08 18:35:14 -0600

    Update Vaca.js

--- js/animal/Vaca.js
+++ js/animal/Vaca.js
@@ -1,23 +1,32 @@
 var Vaca = (
 	function () {
+		function Vaca(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
+                  Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
+                  this.color;
+            /*this.nombre = pnombre;
+            this.edad = pedad;
+            this.altura = paltura;
+            this.peso = ppeso;
+            this.tamanno = ptamanno;*/
+            }
 
-		function Vaca(pnombre) {
-			Animal.call(this, pnombre);
-			this.color;
-		}
-
-		//Heredar los metodos definidos en Animal (prototype)
-		Vaca.prototype = Object.create(Animal.prototype);
-		Vaca.prototype.constructor = Animal;
-
+            //Herencia
+            Vaca.prototype = Object.create(Animal.prototype);
+            Vaca.prototype.constructor = Animal;
+            
 		//Class Methods
-		Vaca.prototype.comer = function () {
-			console.log(this.nombre + ' soy una vaca y estoy comiendo.');
-		}
+            
+            Vaca.prototype.comer = function () {
+			console.log(this.nombre + ': soy una vaca & solo pasto!');
+        }
 
-		Vaca.prototype.brincar = function () {
-			console.log(this.nombre + ' soy una vaca y estoy comiendo.');
-		}
+        Vaca.prototype.caminar = function () {
+			console.log(this.nombre + ': soy una vaca & casi no camino!');
+        }
+
+        Vaca.prototype.beber = function () {
+			console.log(this.nombre + ': soy una vaca & solo bebo agua!');
+        }      
 
 		return Vaca;
 	}
