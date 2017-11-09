d153f1f7a97d2b006e3b03f9f9d003869d3ad56d
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-08 18:38:11 -0600

    Update Gallina.js

--- js/ave/Gallina.js
+++ js/ave/Gallina.js
@@ -1,9 +1,30 @@
 var Gallina = (
 	function () {
-		function Gallina() {
+		function Gallina(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
+                  Ave.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
+            /*this.nombre = pnombre;
+            this.edad = pedad;
+            this.altura = paltura;
+            this.peso = ppeso;
+            this.tamanno = ptamanno;*/
+            }
+
+            Gallina.prototype = Object.create(Ave.prototype);
+            Gallina.prototype.constructor = Ave;
             
-		}
 		//Class Methods
+            
+        Gallina.prototype.comer = function () {
+			console.log(this.nombre + ': soy una gallina & como lo que sea!');
+        }
+
+        Gallina.prototype.caminar = function () {
+			console.log(this.nombre + ': soy una gallina & camino gracioso!');
+        }
+
+        Gallina.prototype.beber = function () {
+			console.log(this.nombre + ': soy una gallina & solo bebo agua!');
+        }      
 
 		return Gallina;
 	}
