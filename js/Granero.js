7611aba11c25bacc2f28e12afdf53d3035d8c6e8
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-08 18:39:51 -0600

    Update Granero.js

--- js/Granero.js
+++ js/Granero.js
@@ -10,14 +10,10 @@ var Granero = (
             this.cantidadTocino = 0;
 		}
 		//Class Methods
-		Granero.prototype.reportarInventario = function (pcantidad) {
-            console.log('Maíz existente: ' + cantidadMaiz);
-            console.log('Leche existente: ' + cantidadLeche);
-            console.log('Agua existente: ' + cantidadAgua);
-            console.log('Pasto existente: ' + cantidadPasto);
-            console.log('Huevos existente: ' + cantidadHuevos);
-            console.log('Alimento existente: ' + cantidadAlimento);
-            console.log('Tocino existente: ' + cantidadTocino);
+		Granero.prototype.reportarInventario = function () {
+                  var mensaje = 'Maíz existente: ' + this.cantidadMaiz + '<br>' + 'Leche existente: ' + this.cantidadLeche + + '<br>' + 'Agua existente: ' + this.cantidadAgua + '<br>' + 'Pasto existente: ' + this.cantidadPasto + '<br>' + 'Huevos existente: ' + this.cantidadHuevos + '<br>' + 'Alimento existente: ' + this.cantidadAlimento + '<br>' + 'Tocino existente: ' + this.cantidadTocino;
+
+                  return mensaje;
 		}
 
 		return Granero;
