56be423db8404dc92b505545adffed99136735d3
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-08 18:31:20 -0600

    Update Cerdo.js

--- js/animal/Cerdo.js
+++ js/animal/Cerdo.js
@@ -1,9 +1,32 @@
 var Cerdo = (
 	function () {
-		function Cerdo() {
+		function Cerdo(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
+                  Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
+                  this.color;
+            /*this.nombre = pnombre;
+            this.edad = pedad;
+            this.altura = paltura;
+            this.peso = ppeso;
+            this.tamanno = ptamanno;*/
+            }
+
+            //Herencia
+            Cerdo.prototype = Object.create(Animal.prototype);
+            Cerdo.prototype.constructor = Animal;
             
-		}
 		//Class Methods
+            
+            Cerdo.prototype.comer = function () {
+			console.log(this.nombre + ': soy un cerdo & solo como alimento!');
+        }
+
+        Cerdo.prototype.caminar = function () {
+			console.log(this.nombre + ': soy un cerdo & camino en el lodo!');
+        }
+
+        Cerdo.prototype.beber = function () {
+			console.log(this.nombre + ': soy un cerdo & solo bebo agua!');
+        }      
 
 		return Cerdo;
 	}
