b048e9947ec3777d432cd1d688e6f7e9b0df98c0
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-08 18:38:43 -0600

    Update Pato.js

--- js/ave/Pato.js
+++ js/ave/Pato.js
@@ -1,9 +1,30 @@
 var Pato = (
 	function () {
-		function Pato() {
+		function Pato(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
+                  Ave.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
+            /*this.nombre = pnombre;
+            this.edad = pedad;
+            this.altura = paltura;
+            this.peso = ppeso;
+            this.tamanno = ptamanno;*/
+            }
+
+            Pato.prototype = Object.create(Ave.prototype);
+            Pato.prototype.constructor = Ave;
             
-		}
 		//Class Methods
+            
+        Pato.prototype.comer = function () {
+			console.log(this.nombre + ': soy un pato & como lo que sea!');
+        }
+
+        Pato.prototype.caminar = function () {
+			console.log(this.nombre + ': soy un pato & camino gracioso!');
+        }
+
+        Pato.prototype.beber = function () {
+			console.log(this.nombre + ': soy un pato & solo bebo agua!');
+        }      
 
 		return Pato;
 	}
