cb5081382954b2db6d87a727df68016c2070fd0d
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-08 18:32:56 -0600

    Update Perro.js

--- js/animal/Perro.js
+++ js/animal/Perro.js
@@ -1,9 +1,30 @@
 var Perro = (
 	function () {
-		function Perro() {
+		function Perro(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
+                  Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
+            /*this.nombre = pnombre;
+            this.edad = pedad;
+            this.altura = paltura;
+            this.peso = ppeso;
+            this.tamanno = ptamanno;*/
+            }
+
+            Perro.prototype = Object.create(Animal.prototype);
+            Perro.prototype.constructor = Animal;
             
-		}
 		//Class Methods
+            
+        Perro.prototype.comer = function () {
+			console.log(this.nombre + ': soy un perro & como lo que sea!');
+        }
+
+        Perro.prototype.caminar = function () {
+			console.log(this.nombre + ': soy un perro & camino gracioso!');
+        }
+
+        Perro.prototype.beber = function () {
+			console.log(this.nombre + ': soy un perro & solo bebo agua!');
+        }      
 
 		return Perro;
 	}
