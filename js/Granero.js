ea45339aee2ee3d1545779979427b5b8815276a9
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-16 18:14:43 -0600

    Update Granero.js

--- js/Granero.js
+++ js/Granero.js
@@ -1,17 +1,17 @@
 var Granero = (
 	function () {
-		function Granero() {
-            this.cantidadMaiz = 0;
-            this.cantidadLeche = 0;
-            this.cantidadAgua = 0;
-            this.cantidadPasto = 0;
-            this.cantidadHuevos = 0;
-            this.cantidadAlimento = 0;
-            this.cantidadTocino = 0;
+		function Granero(pmaiz, pleche, pagua, ppasto, phuevos, palimento, ptocino) {
+            this.cantidadMaiz = pmaiz;
+            this.cantidadLeche = pleche;
+            this.cantidadAgua = pagua;
+            this.cantidadPasto = ppasto;
+            this.cantidadHuevos = phuevos;
+            this.cantidadAlimento = palimento;
+            this.cantidadTocino = ptocino;
 		}
 		//Class Methods
 		Granero.prototype.reportarInventario = function () {
-                  var mensaje = 'Maíz existente: ' + this.cantidadMaiz + '<br>' + 'Leche existente: ' + this.cantidadLeche + + '<br>' + 'Agua existente: ' + this.cantidadAgua + '<br>' + 'Pasto existente: ' + this.cantidadPasto + '<br>' + 'Huevos existente: ' + this.cantidadHuevos + '<br>' + 'Alimento existente: ' + this.cantidadAlimento + '<br>' + 'Tocino existente: ' + this.cantidadTocino;
+                  var mensaje = 'Maíz existente: ' + this.cantidadMaiz + '<br>' + 'Leche existente: ' + this.cantidadLeche + '<br>' + 'Agua existente: ' + this.cantidadAgua + '<br>' + 'Pasto existente: ' + this.cantidadPasto + '<br>' + 'Huevos existente: ' + this.cantidadHuevos + '<br>' + 'Alimento existente: ' + this.cantidadAlimento + '<br>' + 'Tocino existente: ' + this.cantidadTocino;
 
                   return mensaje;
 		}
