954d41b2449aada2bad141c2f62861280ce1a7ed
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-22 21:13:55 -0600

    Update Vaca.js

--- js/animal/Vaca.js
+++ js/animal/Vaca.js
@@ -2,7 +2,12 @@ var Vaca = (
 	function () {
 		function Vaca(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
                   Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
-                  this.color;
+                  this.precio = 500;
+                  this.cantidadDeProducto = 0;
+			// this.velocidadDeProducion = 2 * this.FRAMERATE;
+			this.tiempoDeProduction = 2 * this.FRAMERATE;
+                  this.cantidadDeProductoPorTiempo = 1 * (this.felicidad / 100);
+                  this.tipo = 'vaca';
             /*this.nombre = pnombre;
             this.edad = pedad;
             this.altura = paltura;
@@ -27,7 +32,33 @@ var Vaca = (
         Vaca.prototype.beber = function () {
 			console.log(this.nombre + ': soy una vaca & solo bebo agua!');
         }      
+        
+        Vaca.prototype.producir = function () {
+              
+      };
 
-		return Vaca;
-	}
+      Vaca.prototype.crearProducto = function () {
+            //this.capacidadProduccion
+            //this.cantidadDeProducto = 0;
+            //this.velocidadDeProducion = 0
+
+            if (this.capacidadProduccion >= this.cantidadDeProducto) {
+                  if (this.tiempo <= this.tiempoDeProduction) {
+                        this.cantidadDeProducto += this.cantidadDeProductoPorTiempo;
+                        this.tiempo = 0;
+                        console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
+                  }
+            } else {
+                  //Vace llena
+            }
+
+      }
+
+      Vaca.prototype.update = function () {
+            this.tiempo++;
+            this.crearProducto();
+      }
+
+      return Vaca;
+}
 )();
