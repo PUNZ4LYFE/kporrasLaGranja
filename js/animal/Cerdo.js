6b9b8aa4b11c10a2730f15784ba33c31949be2d4
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-22 21:15:30 -0600

    Update Cerdo.js

--- js/animal/Cerdo.js
+++ js/animal/Cerdo.js
@@ -2,7 +2,12 @@ var Cerdo = (
 	function () {
 		function Cerdo(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
                   Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
-                  this.color;
+                  this.precio = 200; 
+                  this.cantidadDeProducto = 0;
+			// this.velocidadDeProducion = 2 * this.FRAMERATE;
+			this.tiempoDeProduction = 2 * this.FRAMERATE;
+			this.cantidadDeProductoPorTiempo = 1 * (this.felicidad / 100);
+                  this.tipo = 'cerdo';
             /*this.nombre = pnombre;
             this.edad = pedad;
             this.altura = paltura;
@@ -27,6 +32,32 @@ var Cerdo = (
         Cerdo.prototype.beber = function () {
 			console.log(this.nombre + ': soy un cerdo & solo bebo agua!');
         }      
+
+        Cerdo.prototype.producir = function () {
+            
+    };
+
+       Cerdo.prototype.crearProducto = function () {
+          //this.capacidadProduccion
+          //this.cantidadDeProducto = 0;
+          //this.velocidadDeProducion = 0
+
+          if (this.capacidadProduccion >= this.cantidadDeProducto) {
+                if (this.tiempo <= this.tiempoDeProduction) {
+                      this.cantidadDeProducto += this.cantidadDeProductoPorTiempo;
+                      this.tiempo = 0;
+                      console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
+                }
+          } else {
+                //Vace llena
+          }
+
+    }
+
+    Cerdo.prototype.update = function () {
+          this.tiempo++;
+          this.crearProducto();
+    }
 
 		return Cerdo;
 	}
