b02f173599f975ead2d25218770766cb5ea58e91
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-22 21:18:59 -0600

    Update Granero.js

--- js/Granero.js
+++ js/Granero.js
@@ -1,17 +1,18 @@
 var Granero = (
 	function () {
-		function Granero(pmaiz, pleche, pagua, ppasto, phuevos, palimento, ptocino) {
-            this.cantidadMaiz = pmaiz;
-            this.cantidadLeche = pleche;
-            this.cantidadAgua = pagua;
-            this.cantidadPasto = ppasto;
-            this.cantidadHuevos = phuevos;
-            this.cantidadAlimento = palimento;
-            this.cantidadTocino = ptocino;
+		function Granero(pdinero, pmaiz, pleche, pagua, ppasto, phuevos, palimento, ptocino) {
+                  this.cantidadDinero = pdinero;
+                  this.cantidadMaiz = pmaiz;
+                  this.cantidadLeche = pleche;
+                  this.cantidadAgua = pagua;
+                  this.cantidadPasto = ppasto;
+                  this.cantidadHuevos = phuevos;
+                  this.cantidadAlimento = palimento;
+                  this.cantidadTocino = ptocino;
 		}
 		//Class Methods
 		Granero.prototype.reportarInventario = function () {
-                  var mensaje = 'Maíz existente: ' + this.cantidadMaiz + '<br>' + 'Leche existente: ' + this.cantidadLeche + '<br>' + 'Agua existente: ' + this.cantidadAgua + '<br>' + 'Pasto existente: ' + this.cantidadPasto + '<br>' + 'Huevos existente: ' + this.cantidadHuevos + '<br>' + 'Alimento existente: ' + this.cantidadAlimento + '<br>' + 'Tocino existente: ' + this.cantidadTocino;
+                  var mensaje = '<strong>Dinero: </strong>' + this.cantidadDinero + '<br>' + '<strong>Maíz: </strong>' + this.cantidadMaiz + '<br>' + '<strong>Leche: </strong>' + this.cantidadLeche + '<br>' + '<strong>Agua: </strong>' + this.cantidadAgua + '<br>' + '<strong>Pasto: </strong>' + this.cantidadPasto + '<br>' + '<strong>Huevos: </strong>' + this.cantidadHuevos + '<br>' + '<strong>Alimento: </strong>' + this.cantidadAlimento + '<br>' + '<strong>Tocino: </strong>' + this.cantidadTocino;
 
                   return mensaje;
 		}
