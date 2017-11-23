0eef0029695692ceae0e13d864a5f64c8d1ae236
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-22 21:18:14 -0600

    Update Gallina.js

--- js/ave/Gallina.js
+++ js/ave/Gallina.js
@@ -2,6 +2,11 @@ var Gallina = (
 	function () {
 		function Gallina(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
                   Ave.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
+                  this.precio = 100;
+                  // this.velocidadDeProducion = 2 * this.FRAMERATE;
+                  this.tiempoDeProduction = 2 * this.FRAMERATE;
+                  this.cantidadDeProductoPorTiempo = 1 * (this.felicidad / 100);
+                  this.tipo = 'gallina';
             /*this.nombre = pnombre;
             this.edad = pedad;
             this.altura = paltura;
@@ -25,6 +30,33 @@ var Gallina = (
         Gallina.prototype.beber = function () {
 			console.log(this.nombre + ': soy una gallina & solo bebo agua!');
         }      
+
+        Gallina.prototype.producir = function () {
+            
+    };
+
+    Gallina.prototype.crearProducto = function () {
+          //this.capacidadProduccion
+          //this.cantidadDeProducto = 0;
+          //this.velocidadDeProducion = 0
+
+          if (this.capacidadProduccion >= this.cantidadDeProducto) {
+                if (this.tiempo <= this.tiempoDeProduction) {
+                      this.cantidadDeProducto += this.cantidadDeProductoPorTiempo;
+                      this.tiempo = 0;
+                      console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
+                }
+          } else {
+                //Vace llena
+          }
+
+    }
+
+    Gallina.prototype.update = function () {
+          this.tiempo++;
+          this.crearProducto();
+    }
+
 
 		return Gallina;
 	}
