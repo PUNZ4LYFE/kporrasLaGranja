0a00be863b0621bf756ddfed1a83acb0de975785
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-11-26 17:05:45 -0600

    Update Gato.js

--- js/animal/Gato.js
+++ js/animal/Gato.js
@@ -1,9 +1,15 @@
-var Gato = (
+var Cerdo = (
 	function () {
-		function Gato(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
+		function Cerdo(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion) {
                   Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion)
-                  this.color;
-                  this.tipo = 'gato';
+                  this.precio = 200; 
+                  this.cantidadDeProducto = 0;
+                  this.felicidad = 50;
+                  this.apetito = 50;
+			// this.velocidadDeProducion = 2 * this.FRAMERATE;
+			this.tiempoDeProduction = 2 * this.FRAMERATE;
+			this.cantidadDeProductoPorTiempo = 1 * (this.felicidad / 100);
+                  this.tipo = 'cerdo';
             /*this.nombre = pnombre;
             this.edad = pedad;
             this.altura = paltura;
@@ -12,23 +18,49 @@ var Gato = (
             }
 
             //Herencia
-            Gato.prototype = Object.create(Animal.prototype);
-            Gato.prototype.constructor = Animal;
+            Cerdo.prototype = Object.create(Animal.prototype);
+            Cerdo.prototype.constructor = Animal;
             
 		//Class Methods
             
-            Gato.prototype.comer = function () {
-			console.log(this.nombre + ': soy un gato & solo como atún!');
+            Cerdo.prototype.comer = function () {
+			console.log(this.nombre + ': soy un cerdo & solo como alimento!');
         }
 
-        Gato.prototype.caminar = function () {
-			console.log(this.nombre + ': soy un gato & camino con estilo!');
+        Cerdo.prototype.caminar = function () {
+			console.log(this.nombre + ': soy un cerdo & camino en el lodo!');
         }
 
-        Gato.prototype.beber = function () {
-			console.log(this.nombre + ': soy un gato & solo bebo leche!');
+        Cerdo.prototype.beber = function () {
+			console.log(this.nombre + ': soy un cerdo & solo bebo agua!');
         }      
 
-		return Gato;
+        Cerdo.prototype.producir = function () {
+            
+    };
+
+       Cerdo.prototype.crearProducto = function () {
+          //this.capacidadProduccion
+          //this.cantidadDeProducto = 0;
+          //this.velocidadDeProducion = 0
+
+          if (this.capacidadProduccion >= this.cantidadDeProducto) {
+                if (this.tiempo <= this.tiempoDeProduction) {
+                      this.cantidadDeProducto += this.cantidadDeProductoPorTiempo;
+                      this.tiempo = 0;
+                      console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
+                }
+          } else {
+                //Vace llena
+          }
+
+    }
+
+    Cerdo.prototype.update = function () {
+          this.tiempo++;
+          this.crearProducto();
+    }
+
+		return Cerdo;
 	}
 )();
