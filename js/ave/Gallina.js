5b9d7862a54b624c6ccaf13aab43f6021dc0eb46
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-12-04 18:14:59 -0600

    Update Gallina.js

--- js/ave/Gallina.js
+++ js/ave/Gallina.js
@@ -1,6 +1,6 @@
 var Gallina = (
 	function () {
-		function Gallina(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad) {
+		function Gallina(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad, callback) {
                   Ave.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion,pfelicidad)
                   this.precio = 100;
                   this.felicidad = pfelicidad;
@@ -9,6 +9,8 @@ var Gallina = (
                   this.tiempoDeProduction = 2 * this.FRAMERATE;
                   this.cantidadDeProductoPorTiempo = 1 * (this.felicidad / 100);
                   this.tipo = 'gallina';
+                  this.animalDivCallback = callback;
+                  this.addCard();
             /*this.nombre = pnombre;
             this.edad = pedad;
             this.altura = paltura;
@@ -20,18 +22,6 @@ var Gallina = (
             Gallina.prototype.constructor = Ave;
             
 		//Class Methods
-            
-        Gallina.prototype.comer = function () {
-			console.log(this.nombre + ': soy una gallina & como lo que sea!');
-        }
-
-        Gallina.prototype.caminar = function () {
-			console.log(this.nombre + ': soy una gallina & camino gracioso!');
-        }
-
-        Gallina.prototype.beber = function () {
-			console.log(this.nombre + ': soy una gallina & solo bebo agua!');
-        }      
 
         Gallina.prototype.producir = function () {
             
@@ -48,9 +38,9 @@ var Gallina = (
                   this.felicidad--;
                       this.tiempo = 0;
                       console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
+                      this.updateCards();
                 }
           } else {
-                //Vace llena
           }
 
     }
@@ -58,9 +48,41 @@ var Gallina = (
     Gallina.prototype.update = function () {
           this.tiempo++;
           this.crearProducto();
+          this.updateCards();
     }
 
+    Gallina.prototype.addCard = function () {
+      
+            var animalsContainer = document.getElementById('animalsContainer');
+            this.animalDiv = document.createElement('div');
+            animalsContainer.appendChild(this.animalDiv);
+            this.animalDiv.id = this.nombre;
+            this.animalDiv.classList.add('animalCardContainer');
+            
+            this.animalDivTitle = document.createElement('h5');
+            this.animalDivTitle.id = 'title';
+            this.animalDivTitle.innerHTML = this.nombre + '<br>' + 'Felicidad: ' + this.felicidad + '<br>' + 'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' + 'Apetito: ' + this.apetito;
+            this.animalDivTitle.style.textAlign = 'center';
+            this.animalDivTitle.style.display = 'block';
+            this.animalDivTitle.classList.add('nombreAnimal');
+            this.animalDiv.appendChild(this.animalDivTitle);
+            
+            var foto = document.createElement("img");
+            foto.setAttribute("src", "../img/hen.svg");
+            foto.classList.add('svg');
+            this.animalDiv.appendChild(foto);
+            
+            this.animalDiv.addEventListener('click', this.animalDivCallback, false);
+      }
+      
+    Gallina.prototype.updateCards = function () {
+            this.animalDivTitle.innerHTML = this.nombre + '<br>' +
+            'Felicidad: ' + this.felicidad + '<br>' +
+            'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' +
+            'Apetito: ' + this.apetito;
+      }
 
-		return Gallina;
+
+	return Gallina;
 	}
 )();
