8e081c4bde32817aefffc5c0ae9f6a0fa571e15f
Karla Porras <32964275+PUNZ4LYFE@users.noreply.github.com>
2017-12-04 18:13:47 -0600

    Update Vaca.js

--- js/animal/Vaca.js
+++ js/animal/Vaca.js
@@ -1,20 +1,18 @@
 var Vaca = (
 	function () {
-		function Vaca(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad) {
+		function Vaca(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad, callback) {
                   Animal.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad)
-                  this.precio = 500;
-                  this.cantidadDeProducto = 0;
-                  this.felicidad = pfelicidad;
-                  this.apetito = 50;
-			// this.velocidadDeProducion = 2 * this.FRAMERATE;
-			this.tiempoDeProduction = 200 * this.FRAMERATE;
-                  this.cantidadDeProductoPorTiempo = 1 * (this.felicidad / 100);
+                  this.color;
                   this.tipo = 'vaca';
-            /*this.nombre = pnombre;
-            this.edad = pedad;
-            this.altura = paltura;
-            this.peso = ppeso;
-            this.tamanno = ptamanno;*/
+                  this.felicidad = 50;
+                  this.apetito = 50;
+                  /*this.nombre = pnombre;
+                  this.edad = pedad;
+                  this.altura = paltura;
+                  this.peso = ppeso;
+                  this.tamanno = ptamanno;*/
+                  this.animalDivCallback = callback;
+                  this.addCard();
             }
 
             //Herencia
@@ -23,17 +21,7 @@ var Vaca = (
             
 		//Class Methods
             
-            Vaca.prototype.comer = function () {
-			console.log(this.nombre + ': soy una vaca & solo pasto!');
-        }
-
-        Vaca.prototype.caminar = function () {
-			console.log(this.nombre + ': soy una vaca & casi no camino!');
-        }
-
-        Vaca.prototype.beber = function () {
-			console.log(this.nombre + ': soy una vaca & solo bebo agua!');
-        }      
+    
         
         Vaca.prototype.producir = function () {
               
@@ -51,6 +39,7 @@ var Vaca = (
                         //this.cantidadDeProducto += this.cantidadDeProductoPorTiempo;
                         this.tiempo = 0;
                         console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
+                        this.updateCards();
                   }
             } else {
                   //Vaca llena
@@ -61,7 +50,40 @@ var Vaca = (
       Vaca.prototype.update = function () {
             this.tiempo++;
             this.crearProducto();
+            this.updateCards();
       }
+
+      Vaca.prototype.addCard = function () {
+            
+            var animalsContainer = document.getElementById('animalsContainer');
+            this.animalDiv = document.createElement('div');
+            animalsContainer.appendChild(this.animalDiv);
+            this.animalDiv.id = this.nombre;
+            this.animalDiv.classList.add('animalCardContainer');
+            
+            this.animalDivTitle = document.createElement('h5');
+            this.animalDivTitle.id = 'title';
+            this.animalDivTitle.innerHTML = this.nombre + '<br>' + 'Felicidad: ' + this.felicidad + '<br>' + 'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' + 'Apetito: ' + this.apetito;
+            this.animalDivTitle.style.textAlign = 'center';
+            this.animalDivTitle.style.display = 'block';
+            this.animalDivTitle.classList.add('nombreAnimal');
+            this.animalDiv.appendChild(this.animalDivTitle);
+            
+            var foto = document.createElement("img");
+            foto.setAttribute("src", "../img/cow.svg");
+            foto.classList.add('svg');
+            this.animalDiv.appendChild(foto);
+            
+            this.animalDiv.addEventListener('click', this.animalDivCallback, false);
+      }
+            
+      Vaca.prototype.updateCards = function () {
+            this.animalDivTitle.innerHTML = this.nombre + '<br>' +
+            'Felicidad: ' + this.felicidad + '<br>' +
+            'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' +
+            'Apetito: ' + this.apetito;
+      }
+
 
       return Vaca;
 }
