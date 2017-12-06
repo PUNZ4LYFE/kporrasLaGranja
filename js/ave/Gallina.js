0b61e497fbba16f59aa1b2cf6f87c755c71e092d
Esteban Padilla <esteban.padilla@fairplaylabs.com>
2017-12-05 22:24:38 -0600

    Save.

--- js/ave/Gallina.js
+++ js/ave/Gallina.js
@@ -1,7 +1,7 @@
 var Gallina = (
-	function () {
-		function Gallina(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad, callback) {
-                  Ave.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion,pfelicidad)
+      function () {
+            function Gallina(pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad, callback) {
+                  Ave.call(this, pnombre, pedad, paltura, ppeso, ptamanno, pcapacidadEstomago, pcapacidadConsumoAgua, pcapacidadConsumoAlimento, pcapacidadProduccion, ptipodeProduccion, pfelicidad)
                   this.precio = 100;
                   this.felicidad = pfelicidad;
                   this.apetito = 50;
@@ -11,78 +11,79 @@ var Gallina = (
                   this.tipo = 'gallina';
                   this.animalDivCallback = callback;
                   this.addCard();
-            /*this.nombre = pnombre;
-            this.edad = pedad;
-            this.altura = paltura;
-            this.peso = ppeso;
-            this.tamanno = ptamanno;*/
+                  /*this.nombre = pnombre;
+                  this.edad = pedad;
+                  this.altura = paltura;
+                  this.peso = ppeso;
+                  this.tamanno = ptamanno;*/
             }
 
             Gallina.prototype = Object.create(Ave.prototype);
             Gallina.prototype.constructor = Ave;
-            
-		//Class Methods
 
-        Gallina.prototype.producir = function () {
-            
-    };
+            //Class Methods
 
-    Gallina.prototype.crearProducto = function () {
-          //this.capacidadProduccion
-          //this.cantidadDeProducto = 0;
-          //this.velocidadDeProducion = 0
+            Gallina.prototype.producir = function () {
 
-          if (this.capacidadProduccion >= this.cantidadDeProducto) {
-                if (this.tiempo <= this.tiempoDeProduction) {
-                  this.cantidadDeProducto = Math.round(this.cantidadDeProducto + this.cantidadDeProductoPorTiempo);
-                  this.felicidad--;
-                      this.tiempo = 0;
-                      console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
-                      this.updateCards();
-                }
-          } else {
-          }
+            };
 
-    }
+            Gallina.prototype.crearProducto = function () {
+                  //this.capacidadProduccion
+                  //this.cantidadDeProducto = 0;
+                  //this.velocidadDeProducion = 0
 
-    Gallina.prototype.update = function () {
-          this.tiempo++;
-          this.crearProducto();
-          this.updateCards();
-    }
+                  if (this.cantidadDeProducto <= this.capacidadProduccion) {
+                        if (this.tiempo >= this.tiempoDeProduction) {
+                              this.cantidadDeProducto = Math.round(this.cantidadDeProducto + this.cantidadDeProductoPorTiempo);
+                              this.felicidad--;
+                              this.tiempo = 0;
+                              console.log(this.nombre + ' tiene ' + this.cantidadDeProducto + ' de producto!');
+                              this.updateCards();
+                        }
+                  } else {
+                        this.tiempo = 0;
+                  }
 
-    Gallina.prototype.addCard = function () {
-      
-            var animalsContainer = document.getElementById('animalsContainer');
-            this.animalDiv = document.createElement('div');
-            animalsContainer.appendChild(this.animalDiv);
-            this.animalDiv.id = this.nombre;
-            this.animalDiv.classList.add('animalCardContainer');
-            
-            this.animalDivTitle = document.createElement('h5');
-            this.animalDivTitle.id = 'title';
-            this.animalDivTitle.innerHTML = this.nombre + '<br>' + 'Felicidad: ' + this.felicidad + '<br>' + 'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' + 'Apetito: ' + this.apetito;
-            this.animalDivTitle.style.textAlign = 'center';
-            this.animalDivTitle.style.display = 'block';
-            this.animalDivTitle.classList.add('nombreAnimal');
-            this.animalDiv.appendChild(this.animalDivTitle);
-            
-            var foto = document.createElement("img");
-            foto.setAttribute("src", "../img/hen.svg");
-            foto.classList.add('svg');
-            this.animalDiv.appendChild(foto);
-            
-            this.animalDiv.addEventListener('click', this.animalDivCallback, false);
+            }
+
+            Gallina.prototype.update = function () {
+                  this.tiempo++;
+                  this.crearProducto();
+                  this.updateCards();
+            }
+
+            Gallina.prototype.addCard = function () {
+
+                  var animalsContainer = document.getElementById('animalsContainer');
+                  this.animalDiv = document.createElement('div');
+                  animalsContainer.appendChild(this.animalDiv);
+                  this.animalDiv.id = this.nombre;
+                  this.animalDiv.classList.add('animalCardContainer');
+
+                  this.animalDivTitle = document.createElement('h5');
+                  this.animalDivTitle.id = 'title';
+                  this.animalDivTitle.innerHTML = this.nombre + '<br>' + 'Felicidad: ' + this.felicidad + '<br>' + 'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' + 'Apetito: ' + this.apetito;
+                  this.animalDivTitle.style.textAlign = 'center';
+                  this.animalDivTitle.style.display = 'block';
+                  this.animalDivTitle.classList.add('nombreAnimal');
+                  this.animalDiv.appendChild(this.animalDivTitle);
+
+                  var foto = document.createElement("img");
+                  foto.setAttribute("src", "../img/hen.svg");
+                  foto.classList.add('svg');
+                  this.animalDiv.appendChild(foto);
+
+                  this.animalDiv.addEventListener('click', this.animalDivCallback, false);
+            }
+
+            Gallina.prototype.updateCards = function () {
+                  this.animalDivTitle.innerHTML = this.nombre + '<br>' +
+                        'Felicidad: ' + this.felicidad + '<br>' +
+                        'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' +
+                        'Apetito: ' + this.apetito;
+            }
+
+
+            return Gallina;
       }
-      
-    Gallina.prototype.updateCards = function () {
-            this.animalDivTitle.innerHTML = this.nombre + '<br>' +
-            'Felicidad: ' + this.felicidad + '<br>' +
-            'Cantidad de Producto: ' + this.cantidadDeProducto + '<br>' +
-            'Apetito: ' + this.apetito;
-      }
-
-
-	return Gallina;
-	}
 )();
